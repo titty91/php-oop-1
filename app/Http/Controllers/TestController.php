2af5916c5d0e1e036d4811e38601a45de0da7de1
titty91 <tznbaraldi@gmail.com>
2021-05-21 18:07:25 +0200

    completato

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+class Movie 
+{
+    public $titolo;
+    public $descrizione;
+
+    public function __construct($title, $descriction = null){
+
+        $this -> titolo = $title;
+        $this -> descrizione = $descriction;
+
+    }
+
+    public function getString(){
+
+        return $this -> titolo . $this -> descrizione;
+    }
+}
+
+
 class TestController extends Controller
 {
-    //
+   function home(){
+
+        $movie1 = new Movie('Garfild', ' gatto mangia lasagne');
+        $movie2 = new Movie('Madagascar', ' animali in fuga');
+        $movie1stng = $movie1 -> getString();
+        
+       dd($movie1stng);
+       
+
+        return view('home');
+
+   }
 }
